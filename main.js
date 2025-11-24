ad6e7fbc71792d362923e8d2d93928b269ac1965
Salzi13 <nasgor1479@gmail.com>
2025-11-25 00:34:20 +0700

    Komit Pertama

---
+++ main.js
@@ -0,0 +1,19 @@
+function setDapurLed() {
+  LedDapur.style.backgroundColor="red";
+  ledDapurImage.src="./led-on.png";
+}
+
+function setKamarLed() {
+  LedKamar.style.backgroundColor="red";
+  ledKamarImage.src="./led-on.png";
+}
+
+function setTamuLed() {
+  LedTamu.style.backgroundColor="red";
+  ledTamuImage.src="./led-on.png";
+}
+
+function setToiletLed() {
+  LedToilet.style.backgroundColor="red";
+  ledToiletImage.src="./led-on.png";
+}
