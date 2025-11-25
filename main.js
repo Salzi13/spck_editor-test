1d968bc73e279b0c3c437bbb18f80bd3f3d73aa7
Salzi3 <nasgor1479@gmail.com>
2025-11-25 20:55:00 +0700

    update main.js

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
+const endpoint = "http://192.168.1.8";
+
 function setDapurLed() {
   LedDapur.style.backgroundColor="red";
   ledDapurImage.src="./led-on.png";
 }
 
 function setKamarLed() {
+  fetch(endpoint + "/dapur", {
+    method: "POST"
+  }).then(response => resonse.text()).then(result => console.log(result))
+  
   LedKamar.style.backgroundColor="red";
   ledKamarImage.src="./led-on.png";
 }
